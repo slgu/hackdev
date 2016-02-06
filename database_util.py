98a8cce7226b3d8b8092db5253618b35579b2427
slgu <blackhero98@gmail.com>
2016-02-06 01:36:49 -0500

    fixing bug of db util

--- database_util.py
+++ database_util.py
@@ -1,5 +1,6 @@
 import pymssql
-import MySQLdb
+import _mysql
+
 
 conn = None
 cursor = None
@@ -8,30 +9,37 @@ cursor = None
 def connect():
     global conn
     global cursor
-    srv = '127.0.0.1' #'stockhunter.database.windows.net'
+    srv = 'stockhunter.csfqneogzevi.us-west-2.rds.amazonaws.com:3306' #'stockhunter.database.windows.net'
     database = 'StockHunter'
-    usr = 'root' #'test@stockhunter'
-    psw = '' #'pass123!'
+    usr = 'test' #'test@stockhunter'
+    psw = 'pass123!' #'pass123!'
     # conn = pymssql.connect(server=srv, user=usr, password=psw, database=db)
-    conn = MySQLdb.connect(srv, usr, psw, database)
+    conn = _mysql.connect(host=srv,user=usr,
+                  passwd=psw,db=database)
     cursor = conn.cursor()
 
+def create_tables():
+    pass
+def query_results(statement):
+    cursor.execute(statement)
+    row = cursor.fetchone()
+    res = []
+    while row:
+        res.add([str(item)for item in row])
+        row = cursor.fetchone()
+    return res
 
 def get_stock(company):
-    cursor.execute('SELECT * from ' + 'stocks')
-    row = cursor.fetchone()
-    while row:
-        print row
-        row = cursor.fetchone()
+    query_results('SELECT * from ' + 'stocks' + 'where symbol = ' + company)
+
+
+def get_predict():
+    query_results('SELECT * from ' + 'stocks')
 
 
 def get_news(company):
-    cursor.execute('SELECT * from ' + 'news')
-    row = cursor.fetchone()
-    while row:
-        print row
-        row = cursor.fetchone()
-
+    query_results('SELECT * from ' + 'news' + 'where news = ' + company)
+   
 
 def add_new(symbol, title, url):
     statement = 'INSERT INTO news VALUES (\'' + symbol + '\', \'' + title + '\', \'' + url + '\')'
